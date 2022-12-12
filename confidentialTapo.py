d4b0bd650617ec8a03476f81446f23d7beae3428
Fefel76 <leredusiracus@gmail.com>
2022-12-12 18:36:01 +0100

    Initial commit

---
+++ confidentialTapo.py
@@ -0,0 +1,73 @@
+from pytapo import Tapo
+import logging
+import os
+
+
+def privacyTapo(enabled):
+
+    try:
+        user = os.environ.get('USER_CAM')
+        password = os.environ.get('PWD_CAM')
+        logging.info("++++++++++++++++++++++++recup env")
+    except:
+        logging.error("Erreur de récupération des variables d'environnements ")
+
+    user='biquet.auger'
+    password='kali3460'
+
+    cam1 = "192.168.1.17"  # ip of the camera, example: 192.168.1.52
+    cam2 = "192.168.1.15"  # ip of the camera, example: 192.168.1.52
+    cam3 = "192.168.1.16"  # ip of the camera, example: 192.168.1.52
+
+    try:
+        garage = Tapo(cam2, "admin", password)
+    except:
+        logging.error("Erreur de connexion sur la caméra TAPO du Garage : @IP = %s et login=%s", cam2, user)
+
+    try:
+        piscine = Tapo(cam3, "admin", password)
+    except:
+        logging.error("Erreur de connexion sur la caméra de la Piscine")
+
+    try:
+        salon = Tapo(cam1, user, password)
+    except:
+        logging.error("Erreur de connexion sur la caméra du Salon")
+
+    try:
+        logging.debug("Etat Caméra du Garage : %s",garage.getBasicInfo())
+        logging.debug("Etat Caméra de la Piscine : %s",piscine.getBasicInfo())
+        logging.debug("Etat Caméra du Salon : %s",salon.getBasicInfo())
+    except:
+       logging.warning("Avertissement sur récupération état des caméras")
+
+
+    if (enabled):
+        try:
+            garage.setPrivacyMode(True)
+        except:
+            logging.error("Erreur lors du passage en mode confidentiel de la caméra GARAGE")
+        try:
+            piscine.setPrivacyMode(True)
+        except:
+            logging.error("Erreur lors du passage en mode confidentiel de la caméra PISCINE")
+        try:
+            salon.setPrivacyMode(True)
+        except:
+            logging.error("Erreur lors du passage en mode confidentiel de la caméra SALON")
+    else:
+        try:
+            garage.setPrivacyMode(False)
+        except:
+            logging.error("Erreur lors du passage en mode non onfidentiel de la caméra GARAGE")
+        try:
+            piscine.setPrivacyMode(False)
+        except:
+            logging.error("Erreur lors du passage en mode nonconfidentiel de la caméra PISCINE")
+        try:
+            salon.setPrivacyMode(False)
+            logging.info("Salon prêt !")
+        except:
+            logging.error("Erreur lors du passage en mode non confidentiel de la caméra SALON")
+
+
---
+++ recoCOM.py
@@ -0,0 +1,156 @@
+import time
+import logging
+from flask import Flask, render_template, request
+import json
+import socket
+import requests
+from requests.exceptions import HTTPError
+from confidentialTapo import privacyTapo
+import pickle
+
+#logging.basicConfig(level=logging.DEBUG, filename="./log/callAPI.log", filemode="w")
+
+logging.info("Flask démarré")
+app= Flask(__name__)
+pickle.dump("True", open("./videos/record.txt", "wb"))
+# Variables générales
+ListSMS = {"Maribel": "checked", "Biquet": "checked", "Fouine": "checked"}
+
+# fonction api
+def call_api(url):
+    try:
+
+        response = requests.get(url)
+        response.raise_for_status()
+        logging.debug("Requête : %s \n Statut : %s \n Contenu : %s ", url, response.status_code, response.content)
+
+        # Gestion des exceptions
+    except HTTPError as http_err:
+        logging.error(f'HTTP error occurred: {http_err}')
+        return response.status_code
+    except Exception as err:
+        logging.error(f'Other error occurred: {err}')
+        return response.status_code
+    else:
+        logging.info('Appel réussi de la requête : %s', url)
+        return response.status_code, response.content
+#Simulateur de l'API de gestion de detection
+
+
+
+@app.route("/")
+def hello():
+    status = getStatus()
+
+    return render_template('index.html', status=status, ListSMS=ListSMS)
+
+
+@app.route("/detection/status", methods=['GET'])
+def getStatus():
+
+    with open('./videos/record.txt', 'rb') as f:
+        record = pickle.load(f)
+
+    return record
+
+
+@app.route("/listsms", methods=['GET'])
+def listSMS():
+    if not(request.args.getlist("sms1")):
+        ListSMS["Maribel"]=""
+    else:
+        ListSMS["Maribel"] = "checked"
+
+    if not(request.args.getlist("sms2")):
+        ListSMS["Biquet"]=""
+    else:
+        ListSMS["Biquet"]="checked"
+
+    if not(request.args.getlist("sms3")):
+        ListSMS["Fouine"]=""
+    else:
+        ListSMS["Fouine"] = "checked"
+
+    logging.debug("Checked", ListSMS)
+
+    status = getStatus()
+    return render_template('index.html', status=status, ListSMS=ListSMS)
+
+@app.route("/sms", methods=['GET'])
+def getSMS():
+
+    try:
+        msg=request.args.get("msg")
+    except:
+        msg="test par défaut"
+
+    logging.debug("Message reçu :", msg)
+
+
+    #Maribel
+    if ListSMS["Maribel"]=="checked":
+        logging.info("Envoi SMS à Maribel : %s", msg)
+        call_api("https://smsapi.free-mobile.fr/sendmsg?user=20209358&pass=wIpQpxTkNSSP0P&msg="+msg)
+
+    #Biquet
+    if ListSMS["Biquet"] == "checked":
+        logging.info("Envoi SMS à Biquet : %s", msg)
+        call_api("https://smsapi.free-mobile.fr/sendmsg?user=99937527&pass=pVMGAXaYQLNllS&msg="+msg)
+
+    #Fouine
+    if ListSMS["Fouine"] == "checked":
+        logging.info("Envoi SMS à Fouine : %s", msg)
+        call_api("https://smsapi.free-mobile.fr/sendmsg?user=20226894&pass=HIh0RvwSUqE80x&msg="+msg)
+
+    status = getStatus()
+    return render_template('index.html', status=status, ListSMS=ListSMS)
+
+
+@app.route("/detection/start", methods=['GET'])
+def getStart():
+
+
+    try:
+        privacyTapo(False)
+    except:
+        logging.error("Erreur lors de l'appel fonction privacy de TAPO")
+
+    time.sleep(5)  # décompte de 5 secondes avant activation
+
+    try:
+
+        pickle.dump("True", open("./videos/record.txt", "wb"))
+    except:
+        logging.error("Erreur Pickle pour activer")
+    else:
+        logging.info("Activation option RECORD")
+
+
+    status=getStatus()
+    return render_template('index.html',status=status, ListSMS=ListSMS)
+
+@app.route("/detection/pause", methods=['GET'])
+def getPause():
+
+
+    try:
+        privacyTapo(True)
+    except:
+        logging.debug("Erreur lors de l'appel fonction privacy de TAPO")
+
+    time.sleep(1)
+    try:
+        pickle.dump("False", open("./videos/record.txt", "wb"))
+    except:
+        logging.error("Erreur Pickle pour desactiver")
+    else:
+        logging.info("Désactivation option RECORD")
+
+
+
+    status = getStatus()
+    return render_template('index.html', status=status, ListSMS=ListSMS)
+
+app.run(debug=False, host="0.0.0.0")
+
+
